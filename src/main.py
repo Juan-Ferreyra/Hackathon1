40f68768a7328e85342d4b2421417822bb549931
weboe <vtalpt@gmail.com>
2025-04-15 17:20:49 -0500

    fix: agregando issue 1 (suma) a la funcion calculadora

--- src/main.py
+++ src/main.py
@@ -1,2 +1,5 @@
 def calculate() -> float:
-    pass
+    operation = input("Ingrese una operacion matematica: ")
+    operation = operation.split()
+    if (operation[1] == '+'):
+        return (operation[0]+operation[2])
